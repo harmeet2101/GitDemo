b8ecd5d9a31fa2b0572a8d270c4b44678b745643
harmeet singh <Harmeet.Singh@depotnet.co.uk>
2022-03-01 00:07:57 +0530

    changes

--- src/Second.java
+++ src/Second.java
@@ -6,7 +6,7 @@ public class Second {
     }
 
     public void M2(){
-        System.out.println("test");
+        System.out.println("test123");
     }
 
     public static void main(String[] args) {
